2539a7c25b98bf028e23f1abe41105a6a2210bf7
Stanley Lau <stanleyyylau@gmail.com>
2016-12-30 15:44:33 +0800

    finish all optimization

--- js/year_end.js
+++ js/year_end.js
@@ -1,10 +1,23 @@
-new PageSlider({
-  pages: $('.page-wrap .page')
+var fullPage = new PageSlider({
+  pages: $('.page-wrap .page'),
+  onchange: function () {
+    if(this.index === 0){
+      $(".brush").addClass("cute-brush");
+      $(".year_end").addClass("fadeInRight");
+      $(".sccisors").addClass("fadeInLeft");
+      $(".count-list").addClass("fadeInRight");
+      $(".year_end_list").addClass("bounceInUp");
+      $(".blue-attention").addClass("blue-attention-animated");
+    }else {
+      $(".brush").removeClass("cute-brush");
+      $(".year_end").removeClass("fadeInRight");
+      $(".sccisors").removeClass("fadeInLeft");
+      $(".count-list").removeClass("fadeInRight");
+      $(".year_end_list").removeClass("bounceInUp");
+      $(".blue-attention").removeClass("blue-attention-animated");
+    }
+  }
 });
-
-$(document).ready(function () {
-
-})
 
 window.onload = function(){
   $('.loading').remove();
